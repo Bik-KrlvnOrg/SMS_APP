991911f8f7082d3b81e391ac12c0b0a61b8b4266
bik_krlvn <kelvinbirikorang@gmail.com>
2021-02-04 23:30:03 +0000

    :bug: fix task view orientation

---
+++ core/src/main/java/com/cheise_proj/core/feature/student/academics/task/TaskDetailStudentFragment.kt
@@ -0,0 +1,23 @@
+package com.cheise_proj.core.feature.student.academics.task
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cheise_proj.core.R
+
+
+class TaskDetailStudentFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_task_detail, container, false)
+    }
+
+
+}
